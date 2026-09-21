284009d33603c3b6fcd726332d5ac100e7e759d0
mafecheverri <mfernanda.echeverri@udea.edu.com>
2026-09-21 16:08:37 -0500

    fix: corrección de la ruta del index

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Configuración de la conexión a Aiven MySQL
 const pool = mysql.createPool({
